0733d50de3a4bf6545da4c42bd09924d60df7989
wangkai <17875822605@163.com>
2022-04-15 14:13:47 +0800

    增加åè增加自动寻找书架 自动寻找书本功能

--- test_script.py
+++ test_script.py
@@ -15,33 +15,62 @@ def test_case():
         start_game(G.DEVICE, "com.stardust.spotlight")
         sleep(10)
         poco = poco_connect("localhost", 5001)
-        button = poco("ViewCanvas").offspring("newest_root").child("Viewport").child("Content").child("NormalBookList(Clone)")[0].child("seeall")
-        search_target_boot(poco, button)
-        # poco("seeall").click()
-        # poco("Play").click()
+        # 寻找书架
+        search_book_lis(poco, book_list_name="Diamond-Free Stories For AD", normal_list='NormalBookList(Clone)',
+                        all_button_name='seeall')
+        # 从书架找书本
+        find_book(poco, book_list='BookOSARoot_CellGroupPrefab(Clone)', books_name="see_all_book(Clone)",
+                  target_book_name="Behind Closed Doors")
+        # reset book
+        poco('Reset', type='Node').click()
+        poco('ComfirmBtn', type='Button').wait(2).click()
+        poco('Play', type='Image').click()
     except Exception as e:
         print(f"error {e}")
 
 
-def search_target_boot(poco, button) -> None:
+def find_book(poco, book_list, books_name, target_book_name):
+    """
+
+    :param poco: object
+    :param book_list: 书架名
+    :param books_name: 书架书的view_name
+    :param target_book_name: 目标书本
+    :return:
+    """
+    for item in poco(book_list, type='Node'):
+        for line in item.child(books_name):
+            flag = line.child("Views").child("Book_name")
+            if flag.attr('TMP_Text') == target_book_name:
+                flag.click()
+                break
+
+
+def search_book_lis(poco, book_list_name: str, normal_list: str, all_button_name: str) -> None:
     """
     搜寻合适的书架
     :param poco:
-    :param button:
+    :param book_list_name:
+    :param normal_list:
+    :param all_button_name:
     :return:
     """
-    try:
-        for i in range(10):
-            if button:
-                button.click()
-                break
-            else:
+    index = poco(normal_list, type='Node')
+    for i in range(7):
+        if index and index.child("title").attr('TMP_Text') == book_list_name:
+            sleep(2)
+            index.child(all_button_name).wait(2).click()
+            print(f"Try click the {index.child(all_button_name)}")
+            break
+        else:
+            try:
                 point_a = [0.5, 0.55]
                 center = [0.5, 0.1]
                 poco.swipe(point_a, center)
                 sleep(2)
-    except Exception as e:
-        print(f"error {e}")
+                # index.child(all_button_name).wait(2).click()
+            except Exception as e:
+                print(f"Find book list error -> {e}")
 
 
 def poco_connect(local_host, port):
