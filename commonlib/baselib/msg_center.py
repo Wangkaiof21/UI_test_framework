474d69b4189b908c835eba31dca7fc1a5c581818
wangkai <17875822605@163.com>
2022-04-22 17:12:11 +0800

    åchange excel module

---
+++ commonlib/baselib/msg_center.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/4/21 19:26
+# @Author  : WangKai
+# @Site    : 
+# @File    : msg_center.py
+# @Software: PyCharm
+
+
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/3/4 21:19
+# @Author  : v_bkaiwang
+# @File    : msg_center.py
+# @Software: win10 Tensorflow1.13.1 python3.6.3
+
+import os
+import time
+import logging
+import logging.handlers
+
+# 每个日志大小100m
+LOG_MAX_SIZE = 100 * 1024 * 1024
+_DEFAULT_LOG_LEVEL = logging.INFO
+
+
+class MsgCenter(object):
+    def __init__(self, testcase_name=None, logger_name='iAutos', level=_DEFAULT_LOG_LEVEL):
+        self.case_name = testcase_name
+        self.file_hander = None
+        self.console_header = None
+        # 第一步 创建一个logger
+        self.logger = logging.getLogger(logger_name)
+        self.logger.handlers.clear()
+        self.logger.setLevel(level)  # log 等级总开关
+
+        # 第二部 创建两个hander 用于写入日志文件和打印到控制台
+        date_time = time.strftime("%Y-%m-%d %H-%M-%S", time.localtime())
+
+        # 默认当前目录为根目录
+        log_path = os.getcwd()
+        log_path = os.path.join(log_path, "Test_log")
+        if not os.path.exists(log_path):
+            os.mkdir(log_path)
+        log_name = date_time + "_" + self.case_name + ".log"
+        log_name = os.path.join(log_path, log_name)
+
+        self.file_hander = logging.FileHandler(log_name, mode="w+", encoding='utf-8')
+        self.file_hander.setLevel(level)
+        # STEP2 01
+        self.console_header = logging.StreamHandler()
+        self.console_header.setLevel(level)
+        self.logger.addHandler(self.file_hander)
+        self.logger.addHandler(self.console_header)
+
+    def log_level_set(self, level=logging.INFO):
+        self.file_hander.setLevel(level)
+        self.console_header.setLevel(level)
+
+
+if __name__ == '__main__':
+    msg = MsgCenter(testcase_name='testcase_001')
+    msg.logger.debug('this is a logger debug message')
+    msg.logger.info('this is a logger info message')
+    msg.logger.warning('this is a logger warning message')
+    msg.logger.error('this is a logger error message')
+    msg.logger.critical('this is a logger critical message')
--- test.py
+++ test.py
@@ -1,53 +1,11 @@
+from commonlib.baselib.excel import Excel
+from commonlib.baselib.log_message import LogMessage, LOG_ERROR, LOG_DEBUG, LOG_WARN, LOG_INFO
+from commonlib.baselib.msg_center import MsgCenter
 import os
 
-# from datetime import date
-#
-# user = 'eric_idle'
-# member_since = date(1975, 7, 31)
-#
-# delta = date.today() - member_since
-#
-# print(f'{user=!s}  {delta.days=:,d}')
-#
-# x = 3
-# print(f"{x+1=}")
-
-import os
-import sys
-
-# print("sys.path[0] = ", sys.path[0])
-# print("sys.argv[0] = ", sys.argv[0])
-# print("__file__ = ", __file__)
-# print("os.path.abspath(__file__) = ", os.path.abspath(__file__))
-# print("os.path.realpath(__file__) = ", os.path.realpath(__file__))
-# print("os.path.dirname(os.path.realpath(__file__)) = ",
-#        os.path.dirname(os.path.realpath(__file__)))
-# print("os.path.split(os.path.realpath(__file__)) = ",
-#        os.path.split(os.path.realpath(__file__)))
-# print("os.path.split(os.path.realpath(__file__))[0] = ",
-#        os.path.split(os.path.realpath(__file__))[0])
-# print("os.getcwd() = ", os.getcwd())
-import zipfile
-
-s = {'id': 10003, 'dialog_id': 10001, 'instance_id': 20001, 'dialog_no': '1', 'action_id': 0,
-     'item_list':
-         [{'content': {'id': 11904, 'sound_id': 11904, 'play_type': 11001, 'type': 1, 'playEffect': 0, 'play_count': 1},
-           'type': 15002,
-           'delay': 0,
-           'belong_split': 0,
-           'editorActionId': 8
-           }]
-     }
-"""
-从条目合成页数
-
-item_list对应的value是个list 
-list有多个值 一个type对应多个content
-type和content 是同级别的 
-
-'type': 15002 是动作 ，'content': {}是对应动作可以选择的值
-首先 先要合并dialog_no 相等的list 获取一页之内可以操作的动作 和动作的值，以后空点也要设置一个type 那这样以后就可以方便操作
-"""
+MODULE_NAME = os.path.splitext(os.path.basename(__file__))[0]
+# msg = MsgCenter(MODULE_NAME)
+msg = MsgCenter(MODULE_NAME, level=LOG_DEBUG)
 
 import json
 from operator import itemgetter
@@ -61,14 +19,31 @@ def list_add(test_list: list):
     return node_
 
 
+def list_to_str(d_list):
+    ret_str = ""
+    for line in d_list:
+        for k, v in line.items():
+            ret_str += v + ","
+    return ret_str
+
+
 def read_json_to_files(files_path, action_dict: dict):
     """
+    从条目合成页数
 
+    item_list对应的value是个list
+    list有多个值 一个type对应多个content
+    type和content 是同级别的
+
+    'type': 15002 是动作 ，'content': {}是对应动作可以选择的值
+    首先 先要合并dialog_no 相等的list 获取一页之内可以操作的动作 和动作的值，以后空点也要设置一个type 那这样以后就可以方便操作
     :param files_path:
     :param action_dict:
     :return:
     """
+    book_ = list()
     for fp in files_path:
+        chapter_data = dict()
         with open(fp, "r", encoding="utf-8") as r:
             json_file = str(r.read())
             json_object = json.loads(json_file)
@@ -77,7 +52,6 @@ def read_json_to_files(files_path, action_dict: dict):
             new_action_list = list()
             for date, items in groupby(action_list, key=itemgetter('dialog_no')):
                 new_action_list.append(list(items))
-
             full_action_list = list()
             for index in range(len(new_action_list)):
                 index_ = list()
@@ -93,17 +67,24 @@ def read_json_to_files(files_path, action_dict: dict):
                         content_types.append(content_type)
                     index_.append(content_types)
                     new_ = list_add(index_)
+                    new_ = list_to_str(new_)
                 new_action_list[index][0]["item_list"] = new_
+                # 新增开关
+                new_action_list[index][0]["test_or_not"] = ""
+                # 暂时屏蔽前置条件
+                new_action_list[index][0]["show_pre"] = ""
                 full_action_list.append(new_action_list[index])
             full_action_lists = list()
             for line in full_action_list:
                 full_action_lists.append(line[0])
-            for line in full_action_lists:
-                print(line)
-        print("\n")
+            chapter_name = os.path.basename(fp).split('.')[0]
+            chapter_data[chapter_name] = full_action_lists
+        book_.append(chapter_data)
         """写一个写表的模块 读表的模块 
             封装方法eval()
+             set{'select_list': [{'select_id': 1019000212701,
         """
+    return book_
 
 
 def matching_dictionary_to_language(value: str, action_dict: dict):
@@ -137,9 +118,25 @@ def read_txt_to_dict(fp):
                     key, value = new_.split("=")
                     action_name_dict[value] = key
         except Exception as e:
-            print(f"Change dict error {e}")
+            LogMessage(level=LOG_ERROR, msg=f"Change dict error {e}", module=MODULE_NAME)
             return {}
     return action_name_dict
+
+
+def write_action_boot_data(action_data, excel_fp: str):
+    """
+    写入数据
+    :param action_data:
+    :param excel_fp:
+    :return:
+    """
+    excel = Excel(excel_fp)
+
+    for index in range(len(action_data)):
+        ch_name = str(action_data[index].keys())
+        # print(action_data[index]["dialog_10190001"])
+        for ch, value in action_data[index].items():
+            excel.records_write(ch, records=value, start_row=None)
 
 
 if __name__ == '__main__':
@@ -148,5 +145,7 @@ if __name__ == '__main__':
     path = [path1, path2]
     # txt_path = os.path.abspath(os.path.join(os.getcwd(), "../../action_dict.txt"))
     txt_path = os.path.abspath(os.path.join(os.getcwd(), "action_book.txt"))
+    excel_path = os.path.abspath(os.path.join(os.getcwd(), "action_.xlsx"))
     result_dict = read_txt_to_dict(txt_path)
-    read_json_to_files(path, action_dict=result_dict)
+    results = read_json_to_files(path, action_dict=result_dict)
+    write_action_boot_data(action_data=results, excel_fp=excel_path)
